c23a5d27b1844662c18f83a0e3e57d03d21099b4
Woochan Lee <0528pilly@gmail.com>
2024-10-23 23:24:42 +0900

    Score system

--- js/script.js
+++ js/script.js
@@ -1,62 +1,87 @@
-// 입력 박스
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const inputBox = document.getElementById("userInput")
-    inputBox.addEventListener("keydown", function(event) {
+    const quizImg = document.querySelector(".teenieping")
+    const questionNumText = document.getElementById("questionNum")
+    const scoreText = document.getElementById("score")
+    const hintText = document.getElementById("hint")
+    const submitButton = document.getElementById("submitButton")
+
+    const answers = [
+        "하츄핑", "키키핑", "아잉핑", "부끄핑", "바로핑", "부투핑", "아자핑", "깜빡핑", "띠용핑", "차차핑", "주르핑", "차나핑",
+        "라라핑", "따라핑", "나르핑", "무셔핑", "투투핑", "차캐핑", "떠벌핑", "다조핑", "화나핑", "시러핑", "바네핑", "악동핑",
+        "덜덜핑", "그림핑", "무거핑", "꺼꿀핑", "씽씽핑", "베베핑", "코자핑", "딱풀핑", "모야핑", "토이핑", "또까핑", "플라핑",
+        "노라핑", "노리핑", "해핑", "아휴핑", "똑똑핑", "꽁꽁핑", "찌릿핑", "홀로핑", "앙대핑"
+    ]
+
+    const totalQuestions = answers.length
+
+    let usedNumbers = []
+    let questionNum = 0
+    let score = 0
+    let currentQuestion, hint
+
+    function getRandomNumber() {
+        let randomNumber
+        [...Array(totalQuestions - 1).keys()]
+    
+        do {
+            randomNumber = Math.floor(Math.random() * totalQuestions) + 1
+        } while (usedNumbers.includes(randomNumber))
+    
+        usedNumbers.push(randomNumber)
+        return randomNumber
+    }
+
+    // 이미지 바꾸기
+    async function changeImage(imgElement, src) {
+        try {
+            const image = new Image()
+            image.src = src
+            await image.decode()
+            imgElement.src = src
+        } catch (error) {
+            console.error(`Failed to load image '${src}'`)
+        }
+    }
+
+    // 정답 체크
+    function isCorrectAnswer(answer) {
+        return answers[currentQuestion - 1] === answer
+    }
+
+    // 새 문제 불러오기
+    function loadNewQuestion() {
+        currentQuestion = getRandomNumber()
+        changeImage(quizImg, `teenieping/${currentQuestion}.png`)
+        inputBox.value = ""
+        hint = answers[currentQuestion - 1].replace("핑", "").replace(/./g, "◯") + "핑"
+        updateUI()
+    }
+
+    // 텍스트 업데이트
+    function updateUI() {
+        questionNumText.innerHTML = `Q${questionNum + 1}`
+        scoreText.innerHTML = `${score}/${totalQuestions}`
+        hintText.innerHTML = hint
+    }
+
+    // 정답 입력했을 때
+    function submitAnswer() {
+        const textInput = inputBox.value.trim()
+        if (!textInput) return
+        if (isCorrectAnswer(textInput)) score++
+        questionNum++
+        loadNewQuestion()
+    }
+
+    // 엔터키
+    inputBox.addEventListener("keydown", (event) => {
         if (event.key === "Enter") {
-            enterAnswer()
+            submitAnswer()
         }
     })
+
+    submitButton.addEventListener("click", submitAnswer)
+
+    loadNewQuestion()
 })
-
-// 리스트 만들기
-const count = 45
-const list = []
-
-function getRandomNumber() {
-    let randomNumber
-    if (list.length >= count) {
-        console.log("All numbers have been generated.")
-        return null
-    }
-
-    do {
-        randomNumber = Math.floor(Math.random() * count) + 1
-    } while (list.includes(randomNumber))
-
-    list.push(randomNumber)
-    return randomNumber
-}
-
-function changeImg(img, src) {
-    var image = new Image()
-    image.onload = function() {
-        img.src = this.src
-    }
-    image.onerror = function() {
-        console.error("Image failed to load " + src);
-    }
-    image.src = src;
-}
-
-// 답 입력됐을 때
-function isCorrectAnswer(answer) {
-    const randomNumber = list[list.length - 1]
-    return answers[randomNumber].answer.toLowerCase() === answer.toLowerCase()
-}
-
-function enterAnswer() {
-    var inputText = document.getElementById("userInput").value.trim();
-    if (inputText === "") {
-        return; 
-    }
-
-    const quizImg = document.querySelector(".teenieping");
-    let random = getRandomNumber();
-
-    if (random) {
-        const bgImg = document.getElementsByClassName("bg-image")[0];
-        changeImg(quizImg, `teenieping/${random}.png`);
-
-        document.getElementById("userInput").value = "";
-    }
-}
